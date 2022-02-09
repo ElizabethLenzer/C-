35c2f954b4c43f97bad657bef181709f62fc8cf7
John Doe <elizabeth.lenzer1998@gmail.com>
2022-02-09 13:44:55 -0800

    c#

---
+++ core Assignments/CollectionsPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CollectionsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = new int[10];
+            int[] NewArr = {0,1,2,3,4,5,6,7,8,9};
+
+            string[] myNames = new string[]{"Tim", "Martin", "Nikki", "Sarah"};
+
+            
+        }
+    }
+}
