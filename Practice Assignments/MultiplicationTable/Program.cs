fbf035b62bef3017721d2715b4291e2f3202989e
John Doe <elizabeth.lenzer1998@gmail.com>
2022-02-12 12:37:06 -0800

    fianl

--- Practice Assignments/MultiplicationTable/Program.cs
+++ Practice Assignments/MultiplicationTable/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿﻿using System;
+using System.Text;
 
 namespace MultiplicationTable
 {
@@ -6,15 +7,36 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
-            int[,,] array2D = new int[10,10];
-            {{1,2,3,4,5,6,7,8,9,10},
-            {2,4,6,8,10,12,14,16,18,20},
-            {3,6,9,12,15,18,21,24,27,30},
-            {4,8,12,16,20,24,28,32,36,40},
-            {5,10,15,20,25,30,35,40,45,50},
-            {6,12,18,24,30,36,42,48,54,60},
-            {7,14,21,28,35,42,49,56,63,70},
+            Console.WriteLine("Hello World!");
+            var table = BuildTable(10);
+            RenderTable(table);
+        }
+        public static int[,] BuildTable(int size)
+        {
+            int[,] table = new int[size,size];
+            for(var row = 0; row < size; row++)
+                for(var col = 0; col < size; col++)
+                    table[row,col] = (row + 1) * (col + 1);
+            return table;
+        }
+        public static void RenderTable(int[,] multiArray)
+        {
+            // StringBuilder class good for mutating strings
+            StringBuilder sb = new StringBuilder();
+
+            // gets length of specified dimention of multi array
+            int size = multiArray.GetLength(0);
+            
+            for(var row = 0; row < size; row++)
+            {
+                sb.Append("[");
+                for(var col = 0; col < size; col++)
+                {
+                    sb.Append($"{multiArray[row,col]}, ");
+                }
+                sb.Append("]\n");
             }
+            Console.WriteLine(sb);
         }
     }
 }
